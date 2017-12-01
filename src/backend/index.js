26ef8fbf24a95bf602f044c2435dd9ca97b6f390
Kira <me@imkira.com>
2017-12-01 17:56:54 +0800

    serve

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,9 +1,17 @@
 import Application from 'koa';
+import Router from 'koa-router';
+import serve from 'koa-static';
 
-const app = new Application();
+function start() {
+  console.log(process.cwd());
+  const app = new Application();
+  const router = new Router();
+  router.get('/', function(ctx) {
+    ctx.response.body = 'test';
+  });
+  app.use(router.routes()).use(router.allowedMethods());
+  app.use(serve('./web/pages/'));
+  app.listen(3000);
+}
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
-});
-
-app.listen(3000);
+start();
